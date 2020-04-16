0648f91b7335791c4cecb8a00fa81c1dc8947b68
Danny Padron <danny.padron09@gmail.com>
2020-04-16 19:45:41 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    return collection[i] if
+yield(collection[i])
+#when this code is run and hits the `yield` line it is going to send whatever is passed in as the argument to the block
+    i += 1
+  end
 end
